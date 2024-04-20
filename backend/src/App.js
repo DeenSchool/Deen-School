6f74d5c1908bae23336be30e42076934431a7ced
memeerzuabir <zubair.javid@zohoo.com>
2024-04-20 16:36:46 +0530

    made :: auth routes

--- backend/src/App.js
+++ backend/src/App.js
@@ -1,18 +1,30 @@
-import 'dotenv/config'
-import ConnectDB from './db/index.js'
-import express from 'express';
+const express = require("express");
+const dotenv = require("dotenv");
+const ConnectDB = require( "./db/index.js");
+const cors = require("cors")
+const userRouter = require("./routes/user.routes");
+const globalErrorHandler = require("./controllers/error.controller");
+dotenv.config({ path: "./.env" });
 
 const app = express();
+app.use(cors());
 
 // Middlewares
 app.use(express.json());
+app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Cannot find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 ConnectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 3000 , ()=>{
-        console.log(`App is listening on Port ${process.env.PORT}`);
-    })
-})
-.catch((err)=>{
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`App is listening on Port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
     console.log("ERROR IN CONNECTION || CONNECTION FAILED", err);
-})
+  });
